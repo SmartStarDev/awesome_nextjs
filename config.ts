54369560af2fcba3783d5050392b5d27e9e0989a
marko <marko.rapacini@outlook.com>
2023-03-16 21:49:55 +0100

    testing

---
+++ config.ts
@@ -0,0 +1,21 @@
+export default {
+    client: {
+        folderName: 'A',
+        gitConfig: {
+            name: 'marko',
+            email: 'marko.rapacini@outlook.com'
+        }
+    },
+    dev: {
+        folderName: 'B',
+        gitConfig: {
+            name: 'marko',
+            email: 'marko.rapacini@outlook.com'
+        }
+    },
+    ignoreList: [
+        '/node_modules',
+        '/.git',
+        '/.idea',
+    ],
+}
